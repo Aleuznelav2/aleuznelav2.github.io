b73ad936a7c3d3f68665a1148dc2ec188f9f235d
Aleuznelav2 <sam.valenzuela@me.com>
2018-06-04 10:22:16 -0400

    Add files via upload

---
+++ miracle/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>The Miracle On Ice</title>
+<link href="global.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Anton|Arimo" rel="stylesheet">
+</head>
+
+<body>
+	<div id="container">
+		<div id="header">
+			<p class="header-title">The Miracle On Ice</p>
+			<div id="menu-bar">
+				<p class="menu-bar-text">Home &nbsp;|&nbsp; Characters &nbsp;|&nbsp; Original Intent &nbsp;|&nbsp; 
+				Immediate Impact &nbsp;|&nbsp; Lasting Impact &nbsp;|&nbsp; Epilogue &nbsp;|&nbsp; Remember When &nbsp;|&nbsp; Works Cited</p>
+			</div>
+		</div>
+		
+		
+	</div>
+</body>
+</html>
